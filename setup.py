6f3e6bfd6f25c11dfde5fa06d95d676c1c8d0e5b
yangdian <2406616734@qq.com>
2022-01-13 09:20:00 +0800

    build

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='StringMatching',
+    version='0.0.1',
+    description='A project for string matching algorithms.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pybitboo/StringMatching.git',
+    author='yangdian',
+    author_email='2406616734@qq.com',
+    keywords='strmatch, stringmatching, sunday,horspool',
+    python_requires='>=3.6, <4',
+)
