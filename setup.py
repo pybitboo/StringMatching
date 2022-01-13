1be1191a662c12c6e444abd8da319b4da9ed5db3
yangdian <2406616734@qq.com>
2022-01-13 10:12:44 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='StringMatching',
-    version='0.0.1',
+    version='0.0.2',
     description='A project for string matching algorithms.',
     long_description=long_description,
     long_description_content_type='text/markdown',
